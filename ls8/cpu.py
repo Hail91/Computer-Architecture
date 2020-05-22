ecd0ddacb6c4a74ad4e34b402d37c2ba8a3f8a0f
Hail91 <aarongillies88@gmail.com>
2020-05-21 20:21:48 -0400

    added functionality for add opcode and call/ret

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,6 +8,9 @@ PRN = 0b01000111
 MUL = 0b10100010
 PUS = 0b01000101
 POP = 0b01000110
+ADD = 0b10100000
+CALL = 0b01010000
+RET = 0b00010001
 # Assign stack pointer to index 7 of register
 SP = 7
 
@@ -19,7 +22,7 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8  # Stack pointer at R7
         self.PC = 0
-        self.reg[SP] = 0xf4 # Set initial value of where stack pointer is pointing (F4 if it's empty)
+        self.reg[SP] = 0XF4 # Set initial value of where stack pointer is pointing (F4 if it's empty)
 
     def ram_read(self, address):
         return self.ram[address]
@@ -44,7 +47,7 @@ class CPU:
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == 'ADD':
+        if op == ADD:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == MUL:
             self.reg[reg_a] *= self.reg[reg_b]
@@ -84,37 +87,38 @@ class CPU:
             operand_b = self.ram_read(self.PC + 2)
 
             if Instruction_reg == HLT:
-                sys.exit()
+                print('EXIT')
+                break
             elif Instruction_reg == LDI:
                 self.reg[operand_a] = operand_b
                 self.PC += 3
+            elif Instruction_reg == ADD:
+                self.alu(ADD, operand_a, operand_b)
+                self.PC += 3
             elif Instruction_reg == PRN:
-                register_num = self.ram[self.PC + 1]
+                register_num = operand_a
                 print(self.reg[register_num])
                 self.PC += 2
             elif Instruction_reg == MUL:
                 self.alu(MUL, operand_a, operand_b)
                 self.PC += 3
             elif Instruction_reg == PUS:
-                # Decrement the stack pointer
+                v = self.reg[operand_a]
                 self.reg[SP] -= 1
-                # Get the reg number we're pushing to
-                reg_key = self.ram[self.PC + 1]
-                # Grab the value out of that register
-                reg_value = self.reg[reg_key]
-                # Store that value at the current stack pointer
-                most_recent_stack_address = self.reg[SP]
-                self.ram[most_recent_stack_address] = reg_value
-                # Increment the PC
+                self.ram_write(v, self.reg[SP])
                 self.PC += 2
             elif Instruction_reg == POP:
-               # 1. Copy the value from the address pointed to by `SP` to the given register.
-               pop_reg_key = self.ram[self.PC + 1]
-               pop_reg_val = self.ram[self.reg[SP]]
-               self.reg[pop_reg_key] = pop_reg_val
-               # 2. Increment `SP`.
+               v = self.ram_read(self.reg[SP])
                self.reg[SP] += 1
+               self.reg[operand_a] = v
                self.PC += 2
+            elif Instruction_reg == CALL:
+                self.reg[SP] -= 1
+                self.ram[self.reg[SP]] = self.PC + 2
+                self.PC = self.reg[operand_a]
+            elif Instruction_reg == RET:
+                self.PC = self.ram[self.reg[SP]]
+                self.reg[SP] += 1
 
 
 
